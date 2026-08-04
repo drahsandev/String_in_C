f4a416988acc310f83131f09db3ca52ce9fb6d84
drahsandev <drahsandev@gmail.com>
2026-08-04 22:50:11 +0500

    all charactr

---
+++ allsmall.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char str[400];
+    printf("Enter string: ");
+    fgets(str,sizeof(str),stdin);
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i]=str[i]+32;
+        }
+       
+    }
+     printf("%s",str);
+    
+    return 0;
+}
